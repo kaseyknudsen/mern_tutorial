ae64bdfbbc85758b6badb76ce6313ba9e57813c7
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-01-11 23:46:07 -0800

    login and logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,49 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">Goal Setter</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            {" "}
-            Login
-            <FaSignInAlt />
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            {" "}
-            Register
-            <FaUser />
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              Logout <FaSignOutAlt />
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                {" "}
+                Login
+                <FaSignInAlt />
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                {" "}
+                Register
+                <FaUser />
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,21 +1,39 @@
 //this is for making the http request
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/users/'
+const API_URL = "/api/users/";
 
 //register user
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData);
+  
+    if (response.data) {
+      localStorage.setItem("user", JSON.stringify(response.data));
     }
+  
+    return response.data;
+  };
 
-    return response.data
-}
+//logout
+const logout = () => {
+  localStorage.removeItem("user");
+};
 
 const authService = {
-    register
-}
+  register,
+  logout,
+  login
+};
 
-export default authService
+export default authService;
