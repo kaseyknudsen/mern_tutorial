050fb746868a24aba595c2b626171ab698aca2f0
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-01-16 01:07:10 -0800

    delete goals

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import goalService from "../goals/goalService";
+import goalService from "./goalService";
 
 //create async thunk fetches data and posts data to and from a REST API
 
@@ -53,6 +53,25 @@ export const getGoals = createAsyncThunk(
   }
 );
 
+//delete user goal
+export const deleteGoal = createAsyncThunk(
+  "goals/delete",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.deleteGoal(id, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const goalSlice = createSlice({
   name: "goal",
   initialState,
@@ -84,6 +103,21 @@ export const goalSlice = createSlice({
         state.goals = action.payload;
       })
       .addCase(getGoals.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(deleteGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals = state.goals.filter(
+          (goal) => goal._id !== action.payload.id
+        );
+      })
+      .addCase(deleteGoal.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,27 +2,59 @@
 //this file checks to make sure the user is logged in before displaying the dashboard
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import GoalItem from "../components/GoalItem";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
-    if(!user) {
-      navigate('/login')
+    if (isError) {
+      console.log(message);
     }
-  }, [user, navigate])
+    if (!user) {
+      navigate("/login");
+    }
+    //this will fetch the goals from the backend and put it in 'goals'
+    dispatch(getGoals());
 
-  return <>
-    <section className="heading">
-      <h1>Welcome {user && user.name}</h1>
-      <p>Goals Dashboard</p>
-    </section>
-    <GoalForm />
-  </>
+    return () => {
+      dispatch(reset());
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+
+      <section className="content">
+       {goals.length > 0 ? (
+        <div className="goals">
+          {goals.map((goal) => (
+            <GoalItem key={goal._id} goal={goal} />
+          ))}
+        </div>
+       ) : (<h3>You have not set any goals</h3>)}
+      </section>
+    </>
+  );
 }
 
 export default Dashboard;
